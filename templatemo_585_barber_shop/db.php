915e404b155cd9be0cccc1bccbed5c647c033e35
Horvy10 <luky.steam1213@gmail.com>
2025-05-19 20:29:58 +0200

    phpDatabazaKoment

--- templatemo_585_barber_shop/db.php
+++ templatemo_585_barber_shop/db.php
@@ -11,3 +11,18 @@ if ($mysqli->connect_error) {
     die('Chyba pripojenia: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 ?>
+
+
+<!-- Takto som si vytvoril databázu cez phpMyAdmin:
+ CREATE DATABASE barber_app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE barber_app;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-->
+
